f8d0d65dfe311e8f54a4f418540e541cd2862825
jesseJV <jesse.j.vargas@hotmail.com>
2023-06-12 20:10:24 -0400

    study question file update

--- StudyQuestions/study_questions_classes.py
+++ StudyQuestions/study_questions_classes.py
@@ -1,25 +1,36 @@
 class User:
-    def __init__(self, id, username) -> None:
-        self._user_id = id
+    def __init__(self, username) -> None:
         self._username = username
+        self._id = 0
         self._question_bank = {}
+        self._next_question_id = 0
 
-    def get_user_id(self):
-        return self._user_id
-    
     def get_username(self):
         return self._username
-    
+
+    def get_user_id(self):
+        return self._id
+
     def get_user_question_bank(self):
         return self._question_bank
-    
-    def add_question_to_user(self, question_object):
-        pass
 
+    def get_next_question_id(self):
+        return self._next_question_id
+
+    def set_user_id(self, id):
+        self._id = id
+
+    def set_next_question_id(self):
+        self._next_question_id += 1
+        return f"{self._id}.{self._next_question_id}"
+
+    def add_question_to_user(self, question_object):
+        self._question_bank[question_object.get_question_id()
+                            ] = question_object
 
 
 class Question:
-    def __init__(self, id, question, answer, subject) -> None:
+    def __init__(self, id, subject, question, answer) -> None:
         self._question_id = id
         self._question = question
         self._answer = answer
@@ -27,25 +38,26 @@ class Question:
 
     def get_question_id(self):
         return self._question_id
-    
+
     def get_question(self):
         return self._question
-    
+
     def get_answer(self):
-        return self._answer 
-    
+        return self._answer
+
     def get_subject(self):
         return self._subject
 
     def get_question_info(self):
         return f"Subject: {self._subject}, Question: {self._question}, Answer: {self._answer}"
 
+
 class Library:
     def __init__(self) -> None:
         self._questions = {}
         self._members = {}
-        self._last_user_id = 000
-        self._last_question_id = 000
+        self._last_user_id = 0
+        self._last_question_id = 0
 
     def get_questions(self):
         return self._questions
@@ -53,21 +65,43 @@ class Library:
     def get_members(self):
         return self._members
 
-    def create_user(self, username):
+    def add_lib_user(self, user_obj):
         self._last_user_id += 1
-        user = User(self._last_user_id, username)
-        self._members[user.get_user_id()] = user
+        user_obj.set_user_id(self._last_user_id)
+        self._members[user_obj.get_username()] = user_obj
+        return f"{user_obj.get_username()} added to library"
 
-    
+    def add_question(self, user, subject, question, answer):
+        if user in self._members:
+            question_id = self._members[user].set_next_question_id()
+            question_obj = question_id
+            question_obj = Question(question_id, subject, question, answer)
+            self._members[user].add_question_to_user(question_obj)
+            self._questions[question_id] = question_obj
 
+
+p1 = User("Jesse")
+p2 = User("Bob")
+p3 = User("Sara")
 
 lib = Library()
-lib.create_user('jesse')
-lib.create_user('bob')
-lib.create_user('sara')
+
+lib.add_lib_user(p1)
+lib.add_lib_user(p2)
+lib.add_lib_user(p3)
 
 print(lib.get_members())
 
+lib.add_question("Jesse", "Math", "What is one plus one?", "Two")
+lib.add_question("Jesse", "Math", "What is two plus two?", "Three")
+lib.add_question("Jesse", "History", "What is today?",
+                 "Monday - Boo Mondays!!!")
+lib.add_question("Jesse", "Misc", "What color is the sun?", "Yellow")
+lib.add_question("Jesse", "Math", "What color is the sky", "Blue")
+
+print(lib.get_questions())
+print(p1.get_user_question_bank())
+
+
 # members = lib.get_members()
 # print(members[1].get_username())
-         
---
+++ StudyQuestions/test.py
@@ -0,0 +1,50 @@
+test_dict = {
+    1: {
+        id: 1,
+        'name': 'Jesse',
+        'question_list': {
+            'Math': {
+                {1: {
+                    'Question1': 'Answer1',
+                    'Question2': 'Answer2',
+                    'Question3': 'Answer3'
+                }
+                }
+            }
+        }
+
+    },
+    2: {
+        id: 2,
+        'name': 'Bob',
+        'question_list': {
+            'Math': {
+                {1: {
+                    'Question1': 'Answer1',
+                    'Question2': 'Answer2',
+                    'Question3': 'Answer3'
+                }
+                }
+            }
+        }
+
+    },
+    3: {
+        id: 3,
+        'name': 'Sara',
+        'question_list': {
+            'Math': {
+                {1: {
+                    'Question1': 'Answer1',
+                    'Question2': 'Answer2',
+                    'Question3': 'Answer3'
+                }
+                }
+            }
+        }
+
+    }
+}
+
+
+print(test_dict)
